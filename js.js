e8dadcbdd8a3b20ac98760976ec9bc78b1f7b736
Habibulloh Husanov <husanovhabibullohhusanov@gmail.com>
2024-05-14 05:33:53 +0500

    Update js.js

--- js.js
+++ js.js
@@ -1,50 +1,44 @@
-var form = document.querySelector('.form')
-var txt = /@/
-var psw = /^[0-9]{4,}$/
-var inpu = document.querySelector('#input1')
-var input = document.querySelector('#input2')
-function T() {
-    if (inpu.value == "" && input.value == "") {
-        alert("The email address and password is empty")
-        return false
-    } else if (inpu.value == "") {
-        alert("The email address is empty")
+var form = document.querySelector('.form');
+const testEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+var email = document.querySelector('#input1');
+var password = document.querySelector('#input2');
+var message = document.querySelector('#message');
+
+// Auth
+
+// if (localStorage.getItem("verified") === true) {
+//     window.location.href = "generate.html";
+// }
+
+// Tests
+
+function Test() {
+    if (email.value === "") {
+        email.classList.add('error');
+        email.classList.remove('ex');
         return false;
-    } else if (input.value == "") {
-        alert("The password is empty")
-        return false
     }
+    if (password.value === "") {
+        password.classList.add('error');
+        password.classList.remove('ex');
+        return false;
+    }
+    return true;
 }
-function Test1() {
-    var inputt = document.getElementById('input1').value
-    if (inputt == "") {
-        inpu.setAttribute('class', "input")
-    }
-    else if (txt.test(inputt)) {
-        inpu.classList.add('ex')
-        inpu.classList.remove('error')
+
+function TestMail() {
+    if (email.value == "") {
+        email.setAttribute('class', "input");
+        return false;
+    } else if (testEmail.test(email.value)) {
+        email.classList.add('ex');
+        email.classList.remove('error');
+        return true;
     } else {
-        inpu.classList.add('error')
-        inpu.classList.remove('ex')
+        email.classList.add('error');
+        email.classList.remove('ex');
+        return false;
     }
 }
-function Test2() {
-    var inputt = document.getElementById('input2').value
-    if (inputt == "") {
-        input.setAttribute('class', "input bot")
-    }
-    else if (psw.test(inputt)) {
-        input.classList.add('ex')
-        input.classList.remove('error')
-    } else {
-        input.classList.add('error')
-        input.classList.remove('ex')
-    }
-}
-inpu.addEventListener("input", Test1)
-input.addEventListener("input", Test2)
-// inpu.addEventListener('keyup', (e) => {
-//     if (e.key == "Backspace") {
-//         Test()
-//     }
-// })
+
+email.addEventListener("input", TestMail);
